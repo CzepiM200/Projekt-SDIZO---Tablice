be3720240fb7e09e42fcd4810ac8e2e072ce5033
CzepiMM <czepimm200@gmail.com>
2019-03-23 23:13:36 +0100

    Add function: Remove_Front

--- SDIZO Projekt 1 - Tablice/SDIZO Projekt 1.cpp
+++ SDIZO Projekt 1 - Tablice/SDIZO Projekt 1.cpp
@@ -1,13 +1,18 @@
 ﻿#include "pch.h"
 #include "Tablica.h"
+#include "Lista.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	Tablica tablica;
-	tablica.Creata_Data(100);
-	tablica.Load_Data();
-	tablica.Print_All();
+	Lista lista;
+	lista.Push_Front(1);
+	lista.Push_Front(1);
+	lista.Push_Front(1);
+	lista.Push_Front(1);
+	lista.Push_Middle(2, 2);
+	lista.Remove_Front();
+	lista.Print_All();
 }
