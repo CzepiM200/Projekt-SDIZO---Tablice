c078f7f9744ec49a61ba06e6cf747863e4a15d80
CzepiMM <czepimm200@gmail.com>
2019-03-25 19:21:32 +0100

    Add functions: Main_Menu_Text, Tablica_Menu_Text and Lista_Menu_Text to SDIZO Projekt 1.cpp

--- SDIZO Projekt 1 - Tablice/SDIZO Projekt 1.cpp
+++ SDIZO Projekt 1 - Tablice/SDIZO Projekt 1.cpp
@@ -28,12 +28,61 @@ void Creata_Data(int size)
 	else cout << "Blad generowania pliku !\n";
 }
 
+void Main_Menu_Text()
+{
+	cout << "==========\n";
+	cout << "== MENU ==\n";
+	cout << "==========\n";
+	cout << "1. Tablica\n";
+	cout << "2. Lista\n";
+	cout << "3. Kopiec Binarny\n";
+	cout << "4. Drzewo czerwono-czarne\n";
+	cout << "==========\n";
+	cout << "5. Generowanie danych\n";
+	cout << "==========\n";
+	cout << "0. Wyjscie\n";
+	cout << "==========\n";
+}
+//cout << "\n";
+void Tablica_Menu_Text()
+{
+	cout << "=============\n";
+	cout << "== TABLICA ==\n";
+	cout << "=============\n";
+	cout << "1. Dodaj na pozatek\n";
+	cout << "2. Dodaj na srodek\n";
+	cout << "3. Dodaj na koniec\n";
+	cout << "4. Usun z poczatku\n";
+	cout << "5. Usun ze srodka\n";
+	cout << "6. Usun z konca\n";
+	cout << "7. Znajdz w tablicy\n";
+	cout << "8. Wyswietl tablice\n";
+	cout << "9. Wczytaj dane do tablicy\n";
+	cout << "=============\n";
+}
+
+void Lista_Menu_Text()
+{
+	cout << "===========\n";
+	cout << "== LISTA ==\n";
+	cout << "===========\n";
+	cout << "1. Dodaj na pozatek\n";
+	cout << "2. Dodaj na srodek\n";
+	cout << "3. Dodaj na koniec\n";
+	cout << "4. Usun z poczatku\n";
+	cout << "5. Usun ze srodka\n";
+	cout << "6. Usun z konca\n";
+	cout << "7. Znajdz w liscie\n";
+	cout << "8. Wyswietl liste\n";
+	cout << "9. Wczytaj dane do listy\n";
+	cout << "===========\n";
+}
+
 int main()
 {
 	//Creata_Data(1000);
-	Lista lista;
-	Tablica tablica;
-	lista.Load_Data();
-	tablica.Load_Data();
-	
+	//Lista lista;
+	//Tablica tablica;
+	//lista.Load_Data();
+	//tablica.Load_Data();
 }
