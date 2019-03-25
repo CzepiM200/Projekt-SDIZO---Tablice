034013a8f6ab37e5081489ef523ae42ecafd88cd
CzepiMM <czepimm200@gmail.com>
2019-03-25 18:50:28 +0100

    Remove function: Creata_Data from Tablica.cpp and Tablica.h

--- SDIZO Projekt 1 - Tablice/Tablica.cpp
+++ SDIZO Projekt 1 - Tablice/Tablica.cpp
@@ -188,26 +188,26 @@ void Tablica::Print_All()
 	}
 }
 
-void Tablica::Creata_Data(int size)
-{
-	srand(time(NULL));
-	ofstream plik("data.txt");
-	cout << "Generowanie pliku z danymi o dlugosci " << size << "..." << endl;
-	fstream data;
-	data.open("data.txt", ios::out | ios::trunc);
-	if (data.is_open() == true)
-	{
-		data << size << endl;
-		for (int i = 0; i < size; i++)
-		{
-			if(rand()%2 == 1) data << rand() * 10000 + rand() << endl;
-			else data << -1*(rand() * 10000 + rand()) << endl;
-		}
-		data.close();
-		cout << "Generowanie pliku zakonczone pomyslnie\n";
-	}
-	else cout << "Blad generowania pliku !\n";
-}
+//void Tablica::Creata_Data(int size)
+//{
+//	srand(time(NULL));
+//	ofstream plik("data.txt");
+//	cout << "Generowanie pliku z danymi o dlugosci " << size << "..." << endl;
+//	fstream data;
+//	data.open("data.txt", ios::out | ios::trunc);
+//	if (data.is_open() == true)
+//	{
+//		data << size << endl;
+//		for (int i = 0; i < size; i++)
+//		{
+//			if(rand()%2 == 1) data << rand() * 10000 + rand() << endl;
+//			else data << -1*(rand() * 10000 + rand()) << endl;
+//		}
+//		data.close();
+//		cout << "Generowanie pliku zakonczone pomyslnie\n";
+//	}
+//	else cout << "Blad generowania pliku !\n";
+//}
 
 void Tablica::Load_Data()
 {
