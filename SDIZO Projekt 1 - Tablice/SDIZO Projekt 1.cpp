984741f84f0788fd892877f8f17e771de33633ce
CzepiMM <czepimm200@gmail.com>
2019-03-25 18:59:04 +0100

    Add function Create_Data to SDIZO Projekt 1.cpp

--- SDIZO Projekt 1 - Tablice/SDIZO Projekt 1.cpp
+++ SDIZO Projekt 1 - Tablice/SDIZO Projekt 1.cpp
@@ -2,15 +2,38 @@
 #include "Tablica.h"
 #include "Lista.h"
 #include <iostream>
+#include <ctime>
+#include <fstream>
 
 using namespace std;
 
+void Creata_Data(int size)
+{
+	srand(time(NULL));
+	ofstream plik("data.txt");
+	cout << "Generowanie pliku z danymi o dlugosci " << size << "..." << endl;
+	fstream data;
+	data.open("data.txt", ios::out | ios::trunc);
+	if (data.is_open() == true)
+	{
+		data << size << endl;
+		for (int i = 0; i < size; i++)
+		{
+			if (rand() % 2 == 1) data << rand() * 10000 + rand() << endl;
+			else data << -1 * (rand() * 10000 + rand()) << endl;
+		}
+		data.close();
+		cout << "Generowanie pliku zakonczone pomyslnie\n";
+	}
+	else cout << "Blad generowania pliku !\n";
+}
+
 int main()
 {
+	//Creata_Data(1000);
 	Lista lista;
-	lista.Push_Back(1);
-	lista.Push_Back(2);
-	lista.Push_Back(3);
-	lista.Push_Back(4);
-	lista.Print_All();
+	Tablica tablica;
+	lista.Load_Data();
+	tablica.Load_Data();
+	
 }
